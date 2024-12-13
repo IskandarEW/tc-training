35be0119d5768ee8566767dbcf1f542ce9fffd70
Iskandar Mamadnazarov <iskandar.mamadnazarov@evolvingweb.com>
2024-12-13 20:08:53 +0000

    Session 3 progress.

---
+++ project/web/themes/custom/transport_canada/components/slider/slider.js
@@ -0,0 +1,29 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+
+    slidesPerView: 1,
+
+    breakpoints: {
+        460: {
+            slidesPerView: 2,
+            spaceBetween: 15,
+        },
+      },
+  });
